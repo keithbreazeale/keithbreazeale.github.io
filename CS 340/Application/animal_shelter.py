6468d7bd76098ea0758beee00f6532e20a72f88d
Keith Breazeale <79427993+keithbreazeale@users.noreply.github.com>
2023-11-21 16:46:39 -0500

    Add files via upload

---
+++ CS 340/Application/animal_shelter.py
@@ -0,0 +1,58 @@
+'''
+Name          : Animal Shelter Database Connector
+Author        : Keith Breazeale
+Date          : 20-Noveber 2023
+Class         : CS-340
+Version       : 2.0
+Purpose       : To provide a database connection to the Grazioso Salvare Animal Shelter Database
+'''
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+class AnimalShelter(object):
+    
+    def __init__(self,username,password):
+    # init connect to mongodb with authentication - originally on port 43981
+        self.client = MongoClient('mongodb://%s:%s@localhost:27017/?authMechanism=DEFAULT&authSource=AAC'% ('aacuser','aacuser'))
+        self.database = self.client['AAC']
+
+#Create Method
+    def create(self, data):
+        if data is not None:
+            self.database.animals.insert(data)  # data should be dictionary
+            return True
+        else:
+            raise Exception("Nothing to save, because data parameter is empty!")
+            return False
+            
+#Read Method
+    def read(self, data):
+        if data is not None:
+            return self.database.animals.find_one(data)
+        else:
+            print("Nothing to read, because data parameter is empty!")
+            return False
+
+#Read All Method        
+    def read_all(self, data):
+        cursor = self.database.animals.find(data, {'_id':False} ) #return cursor pointer to list of results
+        return cursor
+        
+#Update Method
+    def update(self, data, updateData):
+        if data is not None:
+            result = self.database.animals.update_many(data, {"$set" : updateData})
+        else:
+            return "{}"
+        print ("Record Updated!")
+        return result.raw_result
+            
+#Delete Method
+    def delete(self, data):
+        if data is not None:
+            result = self.database.animals.delete_many(data)
+        else:
+            return "{}"
+        print("Record Deleted")
+        return result.raw_result
---
+++ CS 340/Application/test_script.py
@@ -0,0 +1,44 @@
+from animal_shelter import AnimalShelter
+shelter = AnimalShelter("aacuser","aacuser")
+
+data = {"1": 3,
+        "age_upon_outcome" : "4 years",
+        "animal_id" : "27-Mar-2023_test"
+        "animal_type" :"Dog",
+        "breed" : "Bluetick Coonhound",
+        "color" : "Black and White",
+        "date_of_birth" : "2019-01-01",
+        "datetime" : "2019-01-01 00:00:00",
+        "monthyear" : "2019-01-01T00:00:00",
+        "name" : "Courage",
+        "outcome_subtype" : "",
+        "outcome_type" : "Adoption",
+        "sex_upon_outcome" : "Female",
+        "location_lat" : 35.651150,
+        "location_long" : -78.746020,
+        "age_upon_outcome_in_weeks" : 220}
+
+#Create Test
+shelter.create(data):
+    print("Animal added successfully!")
+
+
+#Read Test
+result = shelter.read(data)
+print(result)
+
+#Update Test
+result = shelter.update({"name" : "Courage"}, {"sex_upon_outcome" : "Spayed Female"})
+print(result)
+
+#Confirm update
+result = shelter.read({"name" : "Courage"})
+print(result)
+
+#Delete Test
+result = shelter.delete({"name" : "Courage"})
+print(result)
+
+#Confirm deletion
+result = shelter.read({"name" : "Courage"})
+print(result)
